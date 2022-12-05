d0aec307d0685ff98f28c326fc51dc74a7a8501d
ZuyiI <100923220+ZuyiI@users.noreply.github.com>
2022-12-05 17:45:40 +0800

    Update seamcarver.py

--- seamcarver.py
+++ seamcarver.py
@@ -2,12 +2,13 @@
 
 from picture import Picture
 
+
 class SeamCarver(Picture):
     ## TO-DO: fill in the methods below
     def energy(self, i: int, j: int) -> float:
         if i < 0 or i >= self.width() or j < 0 or j >= self.height():
             raise IndexError
-        
+
         x_gradient = 0
         y_gradient = 0
 
@@ -23,89 +24,93 @@ class SeamCarver(Picture):
 
         return x_gradient + y_gradient
 
-
-    def safe_index(self, x, dim): #wraps index around to avoid having to write if statements for edge cases
+    def safe_index(self, x, dim):  # wraps index around to avoid having to write if statements for edge cases
         if dim == 'w':
             return x % self.width()
         elif dim == 'h':
             return x % self.height()
-            
 
     def find_vertical_seam(self) -> list[int]:
-        
-        M = [[0 for _ in range( self.width() )] for _ in range( self.height() )]
+
+        M = [[0 for _ in range(self.width())] for _ in range(self.height())]
 
         for i in range(self.width()):
             M[0][i] = self.energy(i, 0)
 
         for j in range(1, self.height()):
             for i in range(self.width()):
-                if i == 0: #left edge
-                    #print("L")
-                    M[j][i] = self.energy(i, j) + min( M[j - 1][i], M[j - 1][i + 1] )
-                elif i == self.width() - 1: #right edge
-                    #print("R") 
-                    M[j][i] = self.energy(i, j) + min( M[j - 1][i - 1], M[j - 1][i] )
+                if i == 0:  # left edge
+                    # print("L")
+                    M[j][i] = self.energy(i, j) + min(M[j - 1][i], M[j - 1][i + 1])
+                elif i == self.width() - 1:  # right edge
+                    # print("R")
+                    M[j][i] = self.energy(i, j) + min(M[j - 1][i - 1], M[j - 1][i])
                 else:
-                    #print(i)
-                    M[j][i] = self.energy(i, j) + min( M[j - 1][i - 1], M[j - 1][i], M[j - 1][i + 1] )
+                    # print(i)
+                    M[j][i] = self.energy(i, j) + min(M[j - 1][i - 1], M[j - 1][i], M[j - 1][i + 1])
 
-        #print(M)
+        # print(M)
 
-        min_bottom = min(M[j - self.height()]) #identify the potential seam endings with the lowest values
+        min_bottom = min(M[j - self.height()])  # identify the potential seam endings with the lowest values
         seam_indices = []
         for i in range(self.width()):
             if M[j - self.height()][i] == min_bottom:
-                seam_indices.append([i]) #append index of a minimum bottom value in a list
+                seam_indices.append([i])  # append index of a minimum bottom value in a list
 
-        #print(seam_indices) #seam_indices contains the path of the seam along the minimum values from bottom to top
+        # print(seam_indices) #seam_indices contains the path of the seam along the minimum values from bottom to top
 
         for potential_seam in seam_indices:
-            for j in range(self.height() - 1, 0, -1): #work back up the array along the minimum values, starts at the bottom row and ends at row 1 (second from top)
-                current = potential_seam[-1] #current index of "head" of the seam working up the minimals
-                if current == 0: #left edge
+            for j in range(self.height() - 1, 0,
+                           -1):  # work back up the array along the minimum values, starts at the bottom row and ends at row 1 (second from top)
+                current = potential_seam[-1]  # current index of "head" of the seam working up the minimals
+                if current == 0:  # left edge
                     if M[j - 1][current] <= M[j - 1][current + 1]:
                         potential_seam.append(current)
                     else:
                         potential_seam.append(current + 1)
-                        
-                elif current == self.width() - 1: #right edge
+
+                elif current == self.width() - 1:  # right edge
                     if M[j - 1][current] <= M[j - 1][current - 1]:
                         potential_seam.append(current)
                     else:
                         potential_seam.append(current - 1)
-                
+
                 else:
-                    if M[j - 1][current] <= M[j - 1][current - 1] and M[j - 1][current] <= M[j - 1][current + 1]: #if c <= l and c <= r
-                        potential_seam.append(current) #then have the next index up be c since it's the minimum out of the 3
-                    elif M[j - 1][current - 1] <= M[j - 1][current] and M[j - 1][current - 1] <= M[j - 1][current + 1]: #if 1 <= c and l <= r
-                        potential_seam.append(current - 1) #then have the next index up be l since it's the minimum out of the 3
-                    elif M[j - 1][current + 1] <= M[j - 1][current] and M[j - 1][current + 1] <= M[j - 1][current - 1]: #if r <= c and r <= l
-                        potential_seam.append(current + 1) #then have the next index up be l since it's the minimum out of the 3
+                    if M[j - 1][current] <= M[j - 1][current - 1] and M[j - 1][current] <= M[j - 1][
+                        current + 1]:  # if c <= l and c <= r
+                        potential_seam.append(
+                            current)  # then have the next index up be c since it's the minimum out of the 3
+                    elif M[j - 1][current - 1] <= M[j - 1][current] and M[j - 1][current - 1] <= M[j - 1][
+                        current + 1]:  # if 1 <= c and l <= r
+                        potential_seam.append(
+                            current - 1)  # then have the next index up be l since it's the minimum out of the 3
+                    elif M[j - 1][current + 1] <= M[j - 1][current] and M[j - 1][current + 1] <= M[j - 1][
+                        current - 1]:  # if r <= c and r <= l
+                        potential_seam.append(
+                            current + 1)  # then have the next index up be l since it's the minimum out of the 3
 
         min_seam_total = 1e100
-        for seam in seam_indices: #find min total valued seam out of all seams
+        for seam in seam_indices:  # find min total valued seam out of all seams
             if sum(seam) < min_seam_total:
                 min_seam_total = sum(seam)
                 final_seam = seam
 
         final_seam.reverse()
-        #print(final_seam)
+        # print(final_seam)
         return final_seam
 
-
-    def transpose(self): #essentially flips rows and cols
+    def transpose(self):  # essentially flips rows and cols
         new = {}
-        
+
         for j in range(self.height()):
             for i in range(self.width()):
                 new[j, i] = self[i, j]
 
-        #print(new)
-        #for key in new:
-            #print(key)
+        # print(new)
+        # for key in new:
+        # print(key)
 
-        temp = self.width() #swap the height and width of the image
+        temp = self.width()  # swap the height and width of the image
         self._width = self._height
         self._height = temp
 
@@ -113,33 +118,29 @@ class SeamCarver(Picture):
         for key in new:
             self[key] = new[key]
 
-        #print(self)
-
+        # print(self)
 
     def find_horizontal_seam(self) -> list[int]:
         self.transpose()
         result = self.find_vertical_seam()
         self.transpose()
         return result
-        
 
     def remove_vertical_seam(self, seam: list[int]):
         if self.width() == 1 or len(seam) != self.height():
             raise SeamError
-        
-        for j in range(self.height()): #deleting each pixel on seam
-            #print(f"Removing pixel ({seam[j]}, {j})")
+
+        for j in range(self.height()):  # deleting each pixel on seam
+            # print(f"Removing pixel ({seam[j]}, {j})")
             del self[seam[j], j]
 
-        for j in range(self.height()): #shift each pixel to the right of seam 1 pixel left
+        for j in range(self.height()):  # shift each pixel to the right of seam 1 pixel left
             for i in range(seam[j] + 1, self.width()):
-                #print(f"Shifting pixel ({i}, {j}) to ({i - 1}, {j})")
+                # print(f"Shifting pixel ({i}, {j}) to ({i - 1}, {j})")
                 self[i - 1, j] = self[i, j]
                 del self[i, j]
 
-        self._width -= 1 #shrink width by 1 since a seam is removed
-
-        raise SeamError(Exception)
+        self._width -= 1  # shrink width by 1 since a seam is removed
 
 
     def remove_horizontal_seam(self, seam: list[int]):
@@ -149,8 +150,6 @@ class SeamCarver(Picture):
         self.remove_vertical_seam(seam)
         self.transpose()
 
-        raise SeamError(Exception)
-        
 
 class SeamError(Exception):
     pass
